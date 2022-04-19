ce8fd802e697b7abf08f73ff76637d45f1232153
Abnel Lluberes Mustafa <alluberes@gmail.com>
2022-04-18 20:29:45 -0400

    including routes

--- src/index.js
+++ src/index.js
@@ -1,158 +1,24 @@
-import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-
-function Square (props){
+export default function App() {
     return (
-        <button 
-            className="square" 
-            onClick={props.onClick}>
-            {props.value}
-        </button>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="blogs" element={<Blogs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-
 }
 
-class Board extends React.Component{
-    renderSquare(i){
-        return <Square 
-            value={this.props.squares[i]}
-            onClick={()=> this.props.onClick(i)} />;
-    }
-
-    render(){
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-
-
-        );
-    }
-}
-
-class Game extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            stepNumber: 0,
-            xIsNext: true,
-        }
-    }
-    handleClick(i){
-        const history = this.state.history.slice(0, this.state.stepNumber+2);
-        const current = history[history.length -1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat[{
-                squares: squares,
-            }],
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    jumpTo(step){
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        });
-    }
-
-
-    render(){
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-            const desc = move ? 
-                'Go to move #' + move : 
-                'Go to game start';
-
-            return (
-                <li key={move} className="row">
-                    <button onClick={()=> this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
-        });
-
-        let status;
-        if(winner){
-            status = 'Winner: ' + winner;
-        } else {
-            status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
-        }
-        return (
-            <div>
-                <div className="header">
-                  <h1>a52 </h1>
-                </div>
-                <div className="detail">
-                    <div className="game">
-                        <div className="game-board">
-                            <Board
-                                squares={current.squares}
-                                onClick={(i)=>this.handleClick(i)}
-                            />
-                        </div>
-                        <div className="game-info">
-                            <div>{status}</div>
-                            <div>Step Number: {this.state.stepNumber}</div>
-                            <ol>{moves}</ol>
-                        </div>
-                    </div>
-                </div>                
-            </div>
-            
-            
-        );
-    }
-}
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
-
-function calculateWinner(squares){
-    const lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ];
-    for(let i=0; i<lines.length;i++){
-        const [a,b,c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a];
-        }
-    }
-    return null;
-}
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque euismod, urna eu tincidunt consectetur,
+                nisi nisl aliquam eros, eget tincidunt nisl nunc eu
+                lectus.
+            </p>
+        </div>
+    );
+}
+
+export default Contact;
